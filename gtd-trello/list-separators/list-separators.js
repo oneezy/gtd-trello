6c77c6ac6446fc6f142cd095e1461dfc2840d50a
Justin O'Neill <mrrioku@yahoo.com>
2016-02-17 23:00:54 -0600

    made small adjustments

--- gtd-trello/list-separators/list-separators.js
+++ gtd-trello/list-separators/list-separators.js
@@ -7,9 +7,6 @@
       // Add "right" separator class
       $('h2:contains("(SEPARATOR: R)")').parents('.js-list').addClass('gtd-separator-r');
 
-      // Remove "right" separator class
-      $('h2:not(:contains("(SEPARATOR: R)"))').parents('.js-list').removeClass('gtd-separator-r');
-
       // Remove Text...
       $('h2:contains("(SEPARATOR: R)")').each(function(){
          $(this).html($(this).html().split(" (SEPARATOR: R)").join(""));
