9d7af29641a5b4d067b694fec545ac51a9da63c8
Justin O'Neill <mrrioku@yahoo.com>
2016-03-17 07:47:49 -0600

    added body background color to 'gtd-tab' and also fine-tuned 'hover effects for buttons inside

--- gtd-trello/board-tabs/board-tabs.js
+++ gtd-trello/board-tabs/board-tabs.js
@@ -7,7 +7,11 @@
    function GTD_BoardTabs(){
 
       var   gtdTab   =  ".gtd-trello .board-header";
+      var   gtdBG    =  $( "body" ).attr( "style" );
 
-
+      // Creating...
       $(gtdTab).wrapInner( '<div class="gtd-tab"></div>' );
+
+      // Applying body background color to tab
+      $( ".gtd-tab" ).attr("style", gtdBG);
    }
