ccd5305c08633206653d4a09878bb3e87e3739c8
Justin O'Neill <mrrioku@yahoo.com>
2016-03-17 08:17:31 -0600

    cleaned up the projects and getting ready for dynamic bg colors

--- gtd-trello/list-projects/list-projects.js
+++ gtd-trello/list-projects/list-projects.js
@@ -1,15 +1,15 @@
 /************************************************************************
-	GTD Trello (SEPARATOR Layout)
+	GTD Trello (List Projects)
 *************************************************************************/
 
    function GTD_ListProjects(){
 
       // Add "right" separator class
-      $('h2:contains(" (PROJECTS)")').parents('.js-list').addClass('gtd-projects');
+      $('h2:contains("(PROJECTS)")').parents('.js-list').addClass('gtd-projects');
 
       // Remove Text...
-      $('h2:contains(" (PROJECTS)")').each(function(){
-         $(this).html($(this).html().split(" (PROJECTS)").join(""));
+      $('h2:contains("(PROJECTS)")').each(function(){
+         $(this).html($(this).html().split("(PROJECTS)").join(""));
       });
 
       // Style placeholder
