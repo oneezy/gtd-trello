97f58c97f8f965b531c204b3fdefd5a82e9026af
Justin O'Neill <mrrioku@yahoo.com>
2016-03-17 08:13:30 -0600

    added all the Trello colors

--- gtd-trello/list-labels/list-labels.js
+++ gtd-trello/list-labels/list-labels.js
@@ -7,31 +7,66 @@
    function GTD_ListLabels() {
 
       // Add class
-      $( 'h2:contains(" (SCRUM: Backlog)")' ).parents('.list').addClass('gtd-backlog');
-      $( 'h2:contains(" (SCRUM: To Do)")' ).parents('.list').addClass('gtd-todo');
-      $( 'h2:contains(" (SCRUM: Doing)")' ).parents('.list').addClass('gtd-doing');
-      $( 'h2:contains(" (SCRUM: Done)")' ).parents('.list').addClass('gtd-done');
-      $( 'h2:contains(" (SCRUM: Blocked)")' ).parents('.list').addClass('gtd-blocked');
+      $( 'h2:contains("(WHITE)")' ).parents('.list').addClass('gtd-white');
+      $( 'h2:contains("(BLUE)")' ).parents('.list').addClass('gtd-blue');
+      $( 'h2:contains("(DARK BLUE)")' ).parents('.list').addClass('gtd-darkblue');
+      $( 'h2:contains("(YELLOW)")' ).parents('.list').addClass('gtd-yellow');
+      $( 'h2:contains("(ORANGE)")' ).parents('.list').addClass('gtd-orange');
+      $( 'h2:contains("(RED)")' ).parents('.list').addClass('gtd-red');
+      $( 'h2:contains("(PURPLE)")' ).parents('.list').addClass('gtd-purple');
+      $( 'h2:contains("(PINK)")' ).parents('.list').addClass('gtd-pink');
+      $( 'h2:contains("(GREEN)")' ).parents('.list').addClass('gtd-green');
+      $( 'h2:contains("(DARK GREEN)")' ).parents('.list').addClass('gtd-darkgreen');
+      $( 'h2:contains("(GRAY)")' ).parents('.list').addClass('gtd-gray');
+      $( 'h2:contains("(DARK GRAY)")' ).parents('.list').addClass('gtd-darkgray');
 
       // Remove Text...
-      $( 'h2:contains(" (SCRUM: Backlog)")' ).each(function(){
-         $(this).html($(this).html().split(" (SCRUM: Backlog)").join(""));
+      $( 'h2:contains("(WHITE)")' ).each(function(){
+         $(this).html($(this).html().split("(WHITE)").join(""));
       });
 
-      $( 'h2:contains(" (SCRUM: To Do)")' ).each(function(){
-         $(this).html($(this).html().split(" (SCRUM: To Do)").join(""));
+      $( 'h2:contains("(BLUE)")' ).each(function(){
+         $(this).html($(this).html().split("(BLUE)").join(""));
       });
 
-      $( 'h2:contains(" (SCRUM: Doing)")' ).each(function(){
-         $(this).html($(this).html().split(" (SCRUM: Doing)").join(""));
+      $( 'h2:contains("(DARK BLUE)")' ).each(function(){
+         $(this).html($(this).html().split("(DARK BLUE)").join(""));
       });
 
-      $( 'h2:contains(" (SCRUM: Done)")' ).each(function(){
-         $(this).html($(this).html().split(" (SCRUM: Done)").join(""));
+      $( 'h2:contains("(YELLOW)")' ).each(function(){
+         $(this).html($(this).html().split("(YELLOW)").join(""));
       });
 
-      $( 'h2:contains(" (SCRUM: Blocked)")' ).each(function(){
-         $(this).html($(this).html().split(" (SCRUM: Blocked)").join(""));
+      $( 'h2:contains("(ORANGE)")' ).each(function(){
+         $(this).html($(this).html().split("(ORANGE)").join(""));
+      });
+
+      $( 'h2:contains("(RED)")' ).each(function(){
+         $(this).html($(this).html().split("(RED)").join(""));
+      });
+
+      $( 'h2:contains("(PURPLE)")' ).each(function(){
+         $(this).html($(this).html().split("(PURPLE)").join(""));
+      });
+
+      $( 'h2:contains("(PINK)")' ).each(function(){
+         $(this).html($(this).html().split("(PINK)").join(""));
+      });
+
+      $( 'h2:contains("(GREEN)")' ).each(function(){
+         $(this).html($(this).html().split("(GREEN)").join(""));
+      });
+
+      $( 'h2:contains("(DARK GREEN)")' ).each(function(){
+         $(this).html($(this).html().split("(DARK GREEN)").join(""));
+      });
+
+      $( 'h2:contains("(GRAY)")' ).each(function(){
+         $(this).html($(this).html().split("(GRAY)").join(""));
+      });
+
+      $( 'h2:contains("(DARK GRAY)")' ).each(function(){
+         $(this).html($(this).html().split("(DARK GRAY)").join(""));
       });
 
 
